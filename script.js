5262cfb5c3a06762c1e97a614e91700841c9fabd
Bruno Berwig <contatobrunoberwig@gmail.com>
2025-05-13 15:08:32 -0300

    Implementação de rota /logs criado

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 const app = express();
 const PORT = 8002;
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
 
 function registrarLog(nomeAluno) {
     const dataHora = new Date().toISOString();
@@ -15,7 +15,21 @@ function registrarLog(nomeAluno) {
         }
     });
 }
+
+app.use(express.json());
+
+app.get('/logs', (req, res) => {
+    fs.readFile('./logs.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo de logs:', err);
+            res.status(500).send('Erro ao ler os logs.');
+        } else {
+            res.send(`<pre>${data}</pre>`);
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando`);
-    registrarLog('João da Silva');
+    registrarLog('Bruno');
 });
